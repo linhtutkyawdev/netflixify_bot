150a45954e96813848cac73e29d634cbbeeac2d7
Lin Htut Kyaw <linhtutkyaw.dev@gmail.com>
2024-08-25 21:56:41 +0630

    for the sake of warnning

--- modals.go
+++ modals.go
@@ -7,15 +7,15 @@ type Channel struct {
 }
 
 type Post struct {
-	channel_id       int
-	title            string
-	rating           int
-	description      string
-	tags             string
-	video_id         string
-	video_path       string
-	thumbnail_id     string
-	thumbnail_path   string
-	g_thumbnail_id   string
-	g_thumbnail_path string
+	// channel_id       int
+	// title            string
+	// rating           int
+	// description      string
+	// tags             string
+	video_id string
+	// video_path       string
+	thumbnail_id string
+	// thumbnail_path   string
+	g_thumbnail_id string
+	// g_thumbnail_path string
 }
